e29dfdfeba44328e429c81b70ad5839eb8a21037
Arthur Schreiber <arthurschreiber@github.com>
2018-11-09 09:04:35 -0800

    Update the API.

--- lib/graphql/batch/loader.rb
+++ lib/graphql/batch/loader.rb
@@ -53,7 +53,7 @@ module GraphQL::Batch
         queue << key
 
         unless @scheduled
-          ::Promise::QUEUE.enqueue_macrotask(self)
+          ::Promise::QUEUE.enqueue_macrotask(self, :wait)
           @scheduled = true
         end
 
@@ -76,7 +76,7 @@ module GraphQL::Batch
     end
 
     # For Promise#sync
-    def call #:nodoc:
+    def wait #:nodoc:
       @scheduled = false
 
       if executor
